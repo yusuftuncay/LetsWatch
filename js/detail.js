4526c3c799e51edf88f8d3567e13e8951a4c860f
yusuftuncay <70652416+yusuftuncay@users.noreply.github.com>
2024-11-26 13:55:30 +0100

    Player doesn’t pause when user loses focus on window

--- js/detail.js
+++ js/detail.js
@@ -1064,14 +1064,14 @@ document.addEventListener("DOMContentLoaded", async function () {
     });
 
     // Event listener for beforeunload to handle cases like refreshing the page and page navigation
-    window.addEventListener("beforeunload", () => {
+    //window.addEventListener("beforeunload", () => {
         // Pause the video player
-        player.pause();
-    });
+    //    player.pause();
+    //});
     // Event listener for pagehide to handle cases like closing the tab or switching to another app
-    window.addEventListener("pagehide", () => {
+    //window.addEventListener("pagehide", () => {
         // Pause the video player
-        player.pause();
-    });
+    //    player.pause();
+    //});
 });
 //#endregion
